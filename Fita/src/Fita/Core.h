4a3db7efc7d91c719b5c5fbc1379d63fd70f62d0
Tareq Anuar <MTareqAzim@gmail.com>
2020-05-28 21:27:10 +0800

    Set up premake.

--- Fita/src/Fita/Core.h
+++ Fita/src/Fita/Core.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifdef FI_PLATFORM_WINDOWS
-	#ifdef FITA_BUILD_DLL
+	#ifdef FI_BUILD_DLL
 		#define FITA_API __declspec(dllexport)
 	#else
 		#define FITA_API __declspec(dllimport)
---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Fita"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Fita"
+	location "Fita"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"FI_PLATFORM_WINDOWS",
+			"FI_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "FI_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "FI_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "FI_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Fita/vendor/spdlog/include",
+		"Fita/src"
+	}
+
+	links
+	{
+		"Fita"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"FI_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "FI_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "FI_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "FI_DIST"
+		optimize "On"
